b6cb0a8afad45369354fa329355e64be11298a26
Beenapractice <lalish1975@gmail.com>
2023-05-16 23:18:17 +0100

    updated programme 15 to collect input

--- src/homework/Programme15.java
+++ src/homework/Programme15.java
@@ -1,14 +1,19 @@
 package homework;
 
+import java.util.Scanner;
+
 public class Programme15 {
     /**15. Write a Java program to swap two variables.**/
     public static void main(String[] args){
-        int a = 5;
-        int b = 6;
-        System.out.println("a: "+ a + " b: "+b );
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println("a: "+ a +" b: "+b );
+        Scanner msg = new Scanner(System.in);
+        System.out.println("Enter first variable: ");
+        String msg1= msg.nextLine();
+        System.out.println("Enter second variable: ");
+        String msg2 = msg.nextLine();
+        System.out.println("msg1: "+ msg1 + " msg2: "+ msg2 );
+        String temp = msg1;
+        msg1 = msg2;
+        msg2 = temp;
+        System.out.println("msg1: "+ msg1 + " msg2: "+ msg2 );
     }
 }
